6fc9c2fdb19da199459b189d521a2dfd9ee8b52f
nuthatch <nu7ha7ch@gmail.com>
2018-04-25 11:24:20 +0900

    make shell script to update post to my repository

--- _posts/updatePost.sh
+++ _posts/updatePost.sh
@@ -10,9 +10,9 @@ if [ -z $1 ]; then
 	echo "usage: $0 MESSAGE\n"
 	echo "MESSAGE should have what is difference. \nthanks."
 else
-	MESSAGE="'${@: 1}'" 
+	MESSAGE="${@: 1}" 
 	git add . 
-	git commit -m $MESSAGE 
+	git commit -m "$MESSAGE" 
 	git push
 fi 
 
